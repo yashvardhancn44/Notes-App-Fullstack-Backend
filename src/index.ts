9ed10e54fc61447f6fc79c29125fafdeed09347f
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-21 09:03:57 +0530

    added CRUD operations to endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,70 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
-  res.json({ message: "success!" });
+  const notes = await prisma.note.findMany();
+  res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops something went wrong");
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  const id = parseInt(req.params.id);
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID must be a valid Number");
+  }
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(5000).send("oops something went wrong.");
+  }
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID must be valid integer");
+  }
+  try {
+    await prisma.note.delete({
+      where: { id },
+    });
+    res.status(204).send("Successful");
+  } catch (error) {
+    res.status(500).send("Oops something went wrong");
+  }
 });
 
 app.listen(5000, () => {
